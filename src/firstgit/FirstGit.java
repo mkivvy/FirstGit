e8642ebf24e99b4e8489e2d64b461ff20074bb22
mking@my.wctc.edu <>
2013-01-23 20:44:48 -0600

    first commit

---
+++ src/firstgit/FirstGit.java
@@ -0,0 +1,15 @@
+package firstgit;
+
+/**
+ *
+ * @author Mary
+ */
+public class FirstGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hellow World!");
+    }
+}
