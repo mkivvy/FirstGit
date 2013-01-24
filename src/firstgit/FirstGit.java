355a8f31efdeb25adcfa7c569c415a72bab0b71a
Mary <Mary@10.30.29.237>
2013-01-23 20:52:28 -0600

    reverted to last version

--- src/firstgit/FirstGit.java
+++ src/firstgit/FirstGit.java
@@ -10,7 +10,6 @@ public class FirstGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello students!");
-        System.out.println("Goodbye");
+        System.out.println("Hellow World!");
     }
 }
